a266956625402bf68f4d8c34031f8fc11a552eb8
cabraham1 <christopherabraham8@gmail.com>
2023-01-31 15:59:13 +0100

    Fix eslint errors

--- js/index.js
+++ js/index.js
@@ -3,18 +3,18 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active')
-})
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+  navMenu.classList.toggle('active');
+});
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
 // smooth scrooling implmentation
-const navLinks = document.querySelectorAll("nav a");
+const navLinks = document.querySelectorAll('nav a');
 navLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
+  link.addEventListener('click', (e) => {
     e.preventDefault();
-    const targetSection = document.querySelector(e.target.getAttribute("href"));
-    targetSection.scrollIntoView({ behavior: "smooth" });
+    const targetSection = document.querySelector(e.target.getAttribute('href'));
+    targetSection.scrollIntoView({ behavior: 'smooth' });
   });
 });
