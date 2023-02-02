5403556a65bb7470ea051688fb3c6b80a9f0953f
cabraham1 <christopherabraham8@gmail.com>
2023-02-02 16:39:07 +0100

    Style the modal section and creat javascript objects

---
+++ js/projectDatas.js
@@ -0,0 +1,75 @@
+const projectDatas = [
+  {
+    id: 0,
+    title: 'Tonic',
+    titleDK: 'Tonic',
+    canopy: ['CANOPY', 'Back End Dev', '2015'],
+    dot: '../assets/icons/Counter.svg',
+    class: 'work-container',
+    para1:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    para: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    para2:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tech: ['html', 'css', 'javascript'],
+    imageMob: '../assets/background-img/Snapshoot-Portfolio.svg',
+    imageDesk: './assets/background-img/Snapshoot-Portfolio.png',
+    live: 'https://cabraham1.github.io/',
+    source: 'https://github.com/cabraham1',
+  },
+  {
+    id: 1,
+    title: 'Multi-Post Stories',
+    titleDK: 'Multi-Post Stories',
+    canopy: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    dot: '.././assets/icons/Counter.svg',
+    class: 'work-container work-container-reverse',
+    para1:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    para: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    para2:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends',
+    tech: ['html', 'css', 'javascript'],
+    imageMob: '../assets/background-img/Snapshoot-Portfolio1.svg',
+    imageDesk: '../assets/background-img/Snapshoot-Portfoli.svg',
+    live: 'https://cabraham1.github.io/',
+    source: 'https://github.com/cabraham1',
+  },
+  {
+    id: 2,
+    title: 'Tonic',
+    titleDk: 'Tonic',
+    canopy: ['CANOPY', 'Back End Dev', '2015'],
+    dot: '.././assets/icons/Counter.svg',
+    class: 'work-container',
+    para1:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    para: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    para2:
+      "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR",
+    tech: ['html', 'css', 'javascript'],
+    imageMob: '../assets/background-img/SnapshootPortfolio11.svg',
+    imageDesk: '../assets/background-img/snap-3.svg',
+    live: 'https://cabraham1.github.io/',
+    source: 'https://github.com/cabraham1',
+  },
+  {
+    id: 3,
+    title: 'Multi-Post Stories',
+    canopy: ['Uber Navigation', 'Back End Dev', '2015'],
+    dot: '.././assets/icons/Counter.svg',
+    class: 'work-container work-container-reverse',
+    para1:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    para: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    para2:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tech: ['html', 'css', 'javascript', 'ruby'],
+    imageMob: '../assets/background-img/SnapshootPortfolio-2.svg',
+    imageDesk: '../assets/background-img/Snapshoot.svg',
+    live: 'https://cabraham1.github.io/',
+    source: 'https://github.com/cabraham1',
+  },
+];
+
+export default projectDatas;
